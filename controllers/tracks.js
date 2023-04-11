def2e7474b2284901321682ff22f38a487061376
Raulss74 <68406050+Raulss74@users.noreply.github.com>
2023-04-11 15:04:08 -0600

    Tercer commit

---
+++ controllers/tracks.js
@@ -0,0 +1,31 @@
+const { tracksModel } = require('../models'); 
+
+
+// Obtener lista de la base de datos
+const getItems = async (req, res) => {
+    const data = await tracksModel.find({}); 
+    res.send({data}); 
+}; 
+
+
+// Obtener un detalle
+const getItem = (req, res) => {}; 
+
+// Crear o insertar un registro 
+const createItem = async (req, res) => {
+    const { body } = req
+    console.log(body)
+    const data = await tracksModel.create(body)
+    res.send({data})
+};
+
+// Actualizar un registro 
+const updateItem = (req, res) => {}; 
+
+
+// Eliminar un registro
+const deleteItem = (req, res) => {}; 
+
+
+
+module.exports = { getItems, getItem, createItem, updateItem, deleteItem }; 
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,14 +1,12 @@
 const express = require("express"); 
 const router = express.Router();
+const { getItems, getItem, createItem } = require("../controllers/tracks"); 
 
 
 // TODO http://localhost/tracks GET, POST, DELETE, PUT
 
-router.get("/", (req, res) => {
-    const data = ["Hola, ", "mundo"]; 
-    res.send({data}); 
-})
-
+router.get("/", getItems); 
+router.post("/", createItem);  
 
 
 
