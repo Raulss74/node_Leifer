f60454791b9ff5d7cc0f4af0e9518d0d005438ad
Raulss74 <68406050+Raulss74@users.noreply.github.com>
2023-04-17 14:52:26 -0600

    Quinto commit

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require("express"); 
+const router = express.Router();
+
+
+
+// TODO http://localhost:3001/api/auth/login
+// TODO http://localhost:3001/api/auth/register
+
+router.post("/", );  
+
+
+
+
+
+module.exports = router; 
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,12 +1,17 @@
 const express = require("express"); 
 const router = express.Router();
+const customHeader = require("../middleware/customHeader");
+const { validatorCreateItem } = require("../validators/tracks"); 
 const { getItems, getItem, createItem } = require("../controllers/tracks"); 
 
 
 // TODO http://localhost/tracks GET, POST, DELETE, PUT
 
 router.get("/", getItems); 
-router.post("/", createItem);  
+
+router.post("/", validatorCreateItem, customHeader, createItem);  
+
+
 
 
 
