24e9ec959a635caacf6a38fb94195c591a3347a6
Raulss74 <68406050+Raulss74@users.noreply.github.com>
2023-05-01 14:55:14 -0600

    Séptima

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -1,3 +1,4 @@
+const { matchedData } = require("express-validator");
 const { tracksModel } = require('../models'); 
 const { handleHttpError } = require('../utils/handleError');
 
@@ -15,23 +16,57 @@ const getItems = async (req, res) => {
 
 
 // Obtener un detalle
-const getItem = (req, res) => {}; 
+const getItem = async (req, res) => {
+    try{
+        req = matchedData(req); 
+        const {id} = req; 
+        const data = await tracksModel.findOneDetail(id); 
+        res.send({ data }); 
+    } catch(e){
+        handleHttpError(res, "ERROR_GET_ITEM")
+    }
+}; 
 
 // Crear o insertar un registro 
 const createItem = async (req, res) => {
-    const { body } = req
-    console.log(body)
-    const data = await tracksModel.create(body)
-    res.send({data})
+    try {
+        const body = matchedData(req);        
+        const data = await tracksModel.create(body); 
+        res.status(201); 
+        res.send({data})
+    } catch(e){
+        handleHttpError(res, "ERROR_CREATE_ITEMS");
+    }    
 };
 
 // Actualizar un registro 
-const updateItem = (req, res) => {}; 
+const updateItem = async (req, res) => {
+    try {
+        const {id, ...body} = matchedData(req); 
+        const data = await tracksModel.findOneAndUpdate(
+            id, body
+        ); 
+        res.send({ data });
+    } catch (e) {
+        handleHttpError(res, "ERROR_UPDATE_ITEMS");
+    }
+}; 
 
 
 // Eliminar un registro
-const deleteItem = (req, res) => {}; 
-
-
+const deleteItem = async (req, res) => {
+    try {
+        req=matchedData(req); 
+        const {id} = req; 
+        const deleteResponse = await tracksModel.delete({_id:id}); 
+        const data = {
+            deleted:deleteResponse.matchedCount
+        }
+        res.send({ data }); 
+    } catch (e) {
+        console.log(e)
+        handleHttpError(res, "ERROR_DELETE_ITEM")
+    }
+}; 
 
 module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
--- routes/tracks.js
+++ routes/tracks.js
@@ -1,18 +1,27 @@
 const express = require("express"); 
 const router = express.Router();
 const authMiddleware = require("../middleware/session");
-const { validatorCreateItem } = require("../validators/tracks"); 
-const { getItems, getItem, createItem } = require("../controllers/tracks"); 
+const checkRol = require("../middleware/rol")
+const { validatorCreateItem, validatorGetItem } = require("../validators/tracks"); 
+const { getItems, getItem, createItem, updateItem, deleteItem } = require("../controllers/tracks"); 
 
 
 // TODO http://localhost/tracks GET, POST, DELETE, PUT
 
+// Obtener detalle de los item
 router.get("/", authMiddleware, getItems); 
 
-router.post("/", validatorCreateItem, createItem);  
+// Obtener detalle del item
+router.get("/:id", authMiddleware, validatorGetItem, getItem); 
 
+// Crear un registro 
+router.post("/", authMiddleware, checkRol(['admin']), validatorCreateItem, createItem);  
 
+// Actualizar un registro
+router.put("/:id", authMiddleware, validatorGetItem, validatorCreateItem, updateItem); 
 
+// Obtener detalle del item 
+router.delete("/:id", authMiddleware, validatorGetItem, deleteItem); 
 
 
 module.exports = router; 
